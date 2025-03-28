0e1322794a4b10f1cf0a6f5b4bfedbd8938f06d6
joselucaspj <joselucas.cdc@gmail.com>
2025-03-28 10:00:35 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -4,6 +4,7 @@ import requests
 from io import StringIO
 import os
 import pandas as pd
+from pytz import timezone
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
